1ca6e4b204459a24884ab0cbce5ff88e1647f102
Griffin Lundquist <griffin@arcadiaitpartners.com>
2022-05-31 18:30:22 -0500

    code and file restructer

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Books]
+        }
+    type Books {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    input BookInput {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+  }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    
+    type Query {
+        me: User
+    }
+    type Mutation {
+        loginUser(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput): User
+        removeBook(bookId: Int!): User
+    }
+`
+
+module.exports = typeDefs;
